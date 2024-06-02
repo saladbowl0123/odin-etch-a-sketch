029e7b785709b403e7591fc586df77f7302ed022
saladbowl0123 <saladbowl0123@gmail.com>
2024-06-02 16:47:05 +0800

    add tile hover effect

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let grid = document.querySelector(".grid");
 
-const createTiles = function(height, width) {
+function createTiles (height, width) {
   for (let i = 0; i < height; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
     for (let j = 0; j < width; j++) {
-      const tile = document.createElement("div");
+      let tile = document.createElement("div");
       tile.classList.add("tile");
       row.appendChild(tile);
-      if ((i + j) % 2 == 0) tile.style.background = "black";
+      tile.addEventListener("mouseover",
+        () => tile.style.background = "black");
     }
     grid.appendChild(row);
   }
